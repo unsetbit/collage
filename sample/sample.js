68bd33793c5243cbd0c2ef546eb023abafdde875
Ozan Turgut <ozan@signalfuse.com>
2014-09-08 01:35:54 -0700

    Fixed full page example

--- sample/sample.js
+++ sample/sample.js
@@ -1,22 +1,12 @@
 // Wait for external scripts to be ready
 window.onload = function(){
-	var collage = Collage.create(document.body);
-
-	collage.load('my content', {
-	  flickr: [
-	  	{
-	  		tags: 'popcorn'
-	  	}
-	  ],
-	  googleNews: ['popcorn'],
-	  twitter: [
-	    { query: 'popcorn' },
-	    { query: 'caramel' }
-	  ]
-	}).then(function(){
-		// Wait for the loading to be finished, then
-		// start the collage (filling the center) and set the active tags
-		collage.start('my content');
-		collage.speed(8);
-	});
+  var popcornCollage = collage.create(document.body);
+  popcornCollage.load('popcorn media', {
+    flickr: [{ tags: 'popcorn'}],
+    googleNews: ['popcorn'],
+    reddit: [{ query: 'popcorn' }]
+  }).then(function(){
+    popcornCollage.start('popcorn media');
+    popcornCollage.speed(8);
+  });
 }
